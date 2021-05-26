6a82d249786dc04eeb7f6bc0bddfe5cde483a22b
Luke Morgan <ac107242@avcol.school.nz>
2021-05-26 13:59:25 +1200

    Updated the program to create the database

---
+++ RoomAllocation/Data/DbInitializer.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RoomAllocation.Data
+{
+    public class DbInitializer
+    {
+    }
+}
